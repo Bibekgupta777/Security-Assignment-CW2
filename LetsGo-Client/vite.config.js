5f5f30d8fe3b8988203697ac0a14943ed38bd1f9
Bibekgupta777 <vivek.gupta.22265@gmai.com>
2025-07-25 00:04:33 +0545

    Configure Vite dev server to use HTTPS with local certificates

--- LetsGo-Client/vite.config.js
+++ LetsGo-Client/vite.config.js
@@ -1,14 +1,20 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-// https://vite.dev/config/
+import fs from "fs";
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 4004,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "../LetsGo-Server/cert/localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "../LetsGo-Server/cert/localhost.pem")),
+    },
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
+        target: "https://localhost:5443", // your backend HTTPS URL and port
+        secure: false, // because using self-signed certs
       },
     },
   },
@@ -17,4 +23,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
